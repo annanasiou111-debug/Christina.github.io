bdc398fb3e2961033a7688b13404ffaa5f79da2a
annanasiou111-debug <annanasiou111@gmail.com>
2026-01-22 18:21:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-const form = document.getElementById("wishForm");
-const list = document.getElementById("wishList");
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyB8wTurqBUONktr4f4JpUgiZb1f3SiaKys",
+  authDomain: "christina-baptism.firebaseapp.com",
+  projectId: "christina-baptism",
+  storageBucket: "christina-baptism.appspot.com",
+  messagingSenderId: "392226867008",
+  appId: "1:392226867008:web:7dfd74a8d05c325887dca5"
+};
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  const name = document.getElementById("name").value;
-  const message = document.getElementById("message").value;
+// Services
+const db = firebase.firestore();
+const storage = firebase.storage();
 
-  const div = document.createElement("div");
-  div.className = "wish";
-  div.innerHTML = `<strong>${name}</strong><br>${message}`;
-
-  list.prepend(div);
-  form.reset();
-});
+// ---- TEST ----
+console.log("Firebase connected ✅");
